8871e20b419ca0c3f35d126f9f1b0188bfe0f444
Dave Ortiz <daveortiz@gmail.com>
2012-08-30 16:12:45 -0400

    this actually works on both sides now, ready for real indivo application

--- grails-app/controllers/carra/demographics/CarraOAuthController.groovy
+++ grails-app/controllers/carra/demographics/CarraOAuthController.groovy
@@ -1,19 +1,15 @@
 package carra.demographics
 
-
-
-import org.apache.http.impl.client.DefaultHttpClient
+import oauth.signpost.OAuth
+import oauth.signpost.OAuthProviderListener
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer
 import oauth.signpost.commonshttp.CommonsHttpOAuthProvider
-import oauth.signpost.OAuthProviderListener
 import oauth.signpost.http.HttpRequest
 import oauth.signpost.http.HttpResponse
-import oauth.signpost.OAuth
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpPut
 import org.apache.http.NameValuePair
-import org.apache.http.message.BasicNameValuePair
 import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.message.BasicNameValuePair
 import org.apache.http.protocol.HTTP
 
 /**
@@ -22,25 +18,30 @@ import org.apache.http.protocol.HTTP
 
 class CarraOAuthController {
 
-  private CommonsHttpOAuthConsumer consumer = new CommonsHttpOAuthConsumer("sampleweb@apps.indivo.org", "yourwebapp")
   private String REQUEST_TOKEN_URL = "http://a:8000/oauth/request_token"
   private String ACCESS_TOKEN_URL = "http://a:8000/oauth/access_token"
   private String AUTHENTICATION_URL = "http://a:8080/oauth/authorize"
-
-
 
   def callback = {
     String recordId = params.record_id
     String carenetId = params.carenet_id
 
+    String token = session["token"]
+    String secret = session["secret"]
 
+    if(token != params.oauth_token){
+      render(text: "Tokens don't match, rogue Indivo detected", status: 403)
+    }
+
+    CommonsHttpOAuthConsumer consumer = new CommonsHttpOAuthConsumer("sampleweb@apps.indivo.org", "yourwebapp")
     CommonsHttpOAuthProvider provider = new CommonsHttpOAuthProvider(
             REQUEST_TOKEN_URL,
             ACCESS_TOKEN_URL,
             AUTHENTICATION_URL)
 
-    provider.retrieveAccessToken(consumer, verifier)
-
+    consumer.setTokenWithSecret(token,secret)
+    provider.setOAuth10a(true)
+    String accessToken = provider.retrieveAccessToken(consumer, params.oauth_verifier)
     render("WORKK")
   }
 
@@ -49,31 +50,25 @@ class CarraOAuthController {
   def authenticate = {
     String recordId = params.record_id
     String carenetId = params.carenet_id
-
     String requestTokenURL = null
 
-    //IndivoX requires us to pass record_id or carenet_id on the authorization
-    //for OAUTH
-    if(recordId.length() > 0) {
-      requestTokenURL = REQUEST_TOKEN_URL //+ "?indivo_record_id=${recordId}"
-    }
-    else if(carenetId.length() > 0) {
-      requestTokenURL = REQUEST_TOKEN_URL
-    }
-
+    CommonsHttpOAuthConsumer consumer = new CommonsHttpOAuthConsumer("sampleweb@apps.indivo.org", "yourwebapp")
 
 
     CommonsHttpOAuthProvider provider = new CommonsHttpOAuthProvider(
-            requestTokenURL,
+            REQUEST_TOKEN_URL,
             ACCESS_TOKEN_URL,
             AUTHENTICATION_URL)
 
+
+
+
     provider.setListener(new OAuthProviderListener() {
       void prepareRequest(HttpRequest request) {
-        HttpPost post = (HttpPost) request.unwrap()
-        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        nvps.add(new BasicNameValuePair("indivo_record_id", recordId.toString()))
-        post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+          HttpPost post = (HttpPost) request.unwrap()
+          List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+          nvps.add(new BasicNameValuePair("indivo_record_id", recordId.toString()))
+          post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8))
       }
 
       void prepareSubmission(HttpRequest request) {
@@ -88,6 +83,9 @@ class CarraOAuthController {
 
     provider.setOAuth10a(true)
     String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND)
+    session["token"] = consumer.getToken()
+    session["secret"]  = consumer.getTokenSecret()
+
     return redirect(url: authUrl)
   }
 
