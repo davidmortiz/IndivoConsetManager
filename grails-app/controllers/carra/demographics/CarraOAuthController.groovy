ecff4b9979698c865f4faecdce122d714762df06
Dave Ortiz <daveortiz@gmail.com>
2012-09-04 10:53:10 -0400

    seperated out configuration, made default point to indivo sandbox

--- grails-app/controllers/carra/demographics/CarraOAuthController.groovy
+++ grails-app/controllers/carra/demographics/CarraOAuthController.groovy
@@ -18,9 +18,19 @@ import org.apache.http.protocol.HTTP
 
 class CarraOAuthController {
 
-  private String REQUEST_TOKEN_URL = "http://a:8000/oauth/request_token"
-  private String ACCESS_TOKEN_URL = "http://a:8000/oauth/access_token"
-  private String AUTHENTICATION_URL = "http://a:8080/oauth/authorize"
+
+  private def request_token_url = {
+    "${grailsApplication.config.indivo.indivo_backend}/oauth/request_token"
+  }
+
+  private def access_token_url = {
+    "${grailsApplication.config.indivo.indivo_backend}/oauth/access_token"
+  }
+
+  def authentication_url = {
+    "${grailsApplication.config.indivo.indivo_ui}/oauth/authorize"
+  }
+
 
   def callback = {
     String recordId = params.record_id
@@ -29,17 +39,17 @@ class CarraOAuthController {
     String token = session["token"]
     String secret = session["secret"]
 
-    if(token != params.oauth_token){
+    if(token != params.oauth_token) {
       render(text: "Tokens don't match, rogue Indivo detected", status: 403)
     }
 
     CommonsHttpOAuthConsumer consumer = new CommonsHttpOAuthConsumer("sampleweb@apps.indivo.org", "yourwebapp")
     CommonsHttpOAuthProvider provider = new CommonsHttpOAuthProvider(
-            REQUEST_TOKEN_URL,
-            ACCESS_TOKEN_URL,
-            AUTHENTICATION_URL)
+            request_token_url(),
+            access_token_url(),
+            authentication_url())
 
-    consumer.setTokenWithSecret(token,secret)
+    consumer.setTokenWithSecret(token, secret)
     provider.setOAuth10a(true)
     String accessToken = provider.retrieveAccessToken(consumer, params.oauth_verifier)
     render("WORKK")
@@ -56,19 +66,19 @@ class CarraOAuthController {
 
 
     CommonsHttpOAuthProvider provider = new CommonsHttpOAuthProvider(
-            REQUEST_TOKEN_URL,
-            ACCESS_TOKEN_URL,
-            AUTHENTICATION_URL)
+            request_token_url(),
+            access_token_url(),
+            authentication_url())
 
 
 
 
     provider.setListener(new OAuthProviderListener() {
       void prepareRequest(HttpRequest request) {
-          HttpPost post = (HttpPost) request.unwrap()
-          List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-          nvps.add(new BasicNameValuePair("indivo_record_id", recordId.toString()))
-          post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8))
+        HttpPost post = (HttpPost) request.unwrap()
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        nvps.add(new BasicNameValuePair("indivo_record_id", recordId.toString()))
+        post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8))
       }
 
       void prepareSubmission(HttpRequest request) {
@@ -84,7 +94,7 @@ class CarraOAuthController {
     provider.setOAuth10a(true)
     String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND)
     session["token"] = consumer.getToken()
-    session["secret"]  = consumer.getTokenSecret()
+    session["secret"] = consumer.getTokenSecret()
 
     return redirect(url: authUrl)
   }
