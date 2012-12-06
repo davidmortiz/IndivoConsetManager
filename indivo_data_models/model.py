70868027d25adebcc16ad56958db233f95f340ef
Dave Ortiz <daveortiz@gmail.com>
2012-12-06 11:03:12 -0500

    add comments

--- indivo_data_models/model.py
+++ indivo_data_models/model.py
@@ -1,3 +1,8 @@
+# mkdir ${indivo_server_home}/indivo/data_models/contrib/consent
+# Put this file in ${indivo_server_home}/indivo/data_models/contrib/consent
+#
+#
+
 from indivo.models import Fact
 from django.db import models
 
